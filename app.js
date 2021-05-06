34eab40b5f03a3f7fdd88f4780d261d1ad3ff61c
nickpatsiopoulos <nickpatsiopoulos@hotmail.com>
2021-05-06 19:02:31 -0400

    Small code refactor, improved formatting of lists, added comments, corrected song title typo

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const fs = require('fs')
 //Read the content stored in the yaml file into the "data" object
 const data = yaml.load(fs.readFileSync('./songs.yaml', 'utf-8'));
 
+//Variable declaraiton - warmupNum, mainNum and cooldownNum sets the number of each type of songs to put in the final playlist
 let workoutPlaylist = [];
+const introText = ['', 'Here is your random workout mix:','========================'];
+const warmupNum = 2;
+const mainNum = 6;
+const cooldownNum = 2;
 
 //Helper function - gets [num] number of UNIQUE (i.e. no duplicates) items from the supplied array
 selectRand = (arr, num) => {
@@ -22,15 +27,12 @@ selectRand = (arr, num) => {
 };
 
 //Gets three arrays of songs for each part of the workout playlist - warm up, main workout and cooldown
-let warmupSongs = selectRand(data.warmup, 2);
-let mainWorkoutSongs = selectRand(data.main, 6);
-let cooldownSongs = selectRand(data.cooldown, 2);
+let warmupSongs = selectRand(data.warmup, warmupNum);
+let mainWorkoutSongs = selectRand(data.main, mainNum);
+let cooldownSongs = selectRand(data.cooldown, cooldownNum);
 
-//Assembles the final playlist by concatenating the three arrays above
-workoutPlaylist = [...warmupSongs, ...mainWorkoutSongs, ...cooldownSongs];
+//Assembles the final playlist by concatenating the intro, the three arrays of songs and a line break at the end
+workoutPlaylist = [...introText, ...warmupSongs, ...mainWorkoutSongs, ...cooldownSongs, ...' '];
 
-//Display workout mix
-const introText = 'Here is your random workout mix:';
-workoutPlaylist.splice(0,0,introText);
-
+//Iterates through and displays each item in the console log
 workoutPlaylist.forEach(item => console.log(item));
