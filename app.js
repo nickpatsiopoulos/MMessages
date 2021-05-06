8ec983ff9a220acfc9f984b7ab6f3a21a5d2ccc6
nickpatsiopoulos <nickpatsiopoulos@hotmail.com>
2021-05-06 19:08:18 -0400

    Final UI tweaks and updated comments for clarity

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//Adding access to the node file system and yaml so that data can be stored in a yaml file
+//Adding access to the node file system and yaml so that data can be retrieved from a yaml file
 const yaml = require('js-yaml')
 const fs = require('fs')
 
-//Read the content stored in the yaml file into the "data" object
+//Read the content stored in the local songs.yaml file into a "data" object
 const data = yaml.load(fs.readFileSync('./songs.yaml', 'utf-8'));
 
 //Variable declaraiton - warmupNum, mainNum and cooldownNum sets the number of each type of songs to put in the final playlist
 let workoutPlaylist = [];
-const introText = ['', 'Here is your random workout mix:','========================'];
+const introText = ['', 'Here is your random workout mix:','================================'];
 const warmupNum = 2;
 const mainNum = 6;
 const cooldownNum = 2;
 
-//Helper function - gets [num] number of UNIQUE (i.e. no duplicates) items from the supplied array
+//Helper function - creates a new array of length [num] with just UNIQUE (i.e. no duplicates) items from the supplied array
 selectRand = (arr, num) => {
   let returnCount = 0;
   let newArr = [];
@@ -26,13 +26,13 @@ selectRand = (arr, num) => {
   return newArr;
 };
 
-//Gets three arrays of songs for each part of the workout playlist - warm up, main workout and cooldown
+//Gets three arrays of songs making up each part of the workout playlist - warm up, main workout and cooldown
 let warmupSongs = selectRand(data.warmup, warmupNum);
 let mainWorkoutSongs = selectRand(data.main, mainNum);
 let cooldownSongs = selectRand(data.cooldown, cooldownNum);
 
-//Assembles the final playlist by concatenating the intro, the three arrays of songs and a line break at the end
+//Assembles the final playlist by concatenating the intro, the three arrays of songs and a line break at the end (for readability)
 workoutPlaylist = [...introText, ...warmupSongs, ...mainWorkoutSongs, ...cooldownSongs, ...' '];
 
-//Iterates through and displays each item in the console log
+//Iterates through and displays each item in the console
 workoutPlaylist.forEach(item => console.log(item));
